12d6ae7dc850ae1c54615ac42d9289cf41d5c20c
laborylouis <165661904+laborylouis@users.noreply.github.com>
2025-05-16 15:06:08 +0200

    Menu

--- presentation.cpp
+++ presentation.cpp
@@ -28,22 +28,26 @@ using namespace std;
 //{
 //} //----- Fin de Méthode
 
+#include <iomanip> // For text formatting
+
 void presentation::Menu_principal ( )
 // Algorithme :
 //
 {
-        cout << "|============================================|\n";
-        cout << "|          Menu Principal AirWatcher         |\n";
-        cout << "|============================================|\n";
-        cout << "|[1] -------- Analyse de données ------------|\n";
-        cout << "|[2] -------- Statistiques ------------------|\n";
-        cout << "|[3] -------- Points utilisateurs -----------|\n";
-        cout << "|[4] -------- Administration ----------------|\n";
-        cout << "|[5] -------- Quitter -----------------------|\n";
-        cout << "|============================================|\n";
+    int choix;
+    do
+    {
+        cout << "\033[1;34m|============================================|\033[0m\n";
+        cout << "\033[1;34m|          Menu Principal AirWatcher         |\033[0m\n";
+        cout << "\033[1;34m|============================================|\033[0m\n";
+        cout << "\033[1;32m|[1] -------- Analyse de données ------------|\033[0m\n";
+        cout << "\033[1;32m|[2] -------- Statistiques ------------------|\033[0m\n";
+        cout << "\033[1;32m|[3] -------- Points utilisateurs -----------|\033[0m\n";
+        cout << "\033[1;32m|[4] -------- Administration ----------------|\033[0m\n";
+        cout << "\033[1;31m|[5] -------- Quitter -----------------------|\033[0m\n";
+        cout << "\033[1;34m|============================================|\033[0m\n";
         cout << "Veuillez choisir une valeur entre 1 et 5 : "<< endl;
 
-        int choix;
         cin >> choix;
 
         switch (choix)
@@ -61,40 +65,152 @@ void presentation::Menu_principal ( )
                 Menu_administration();
                 break;
             case 5:
-                cout << "Au revoir !" << endl;
+                cout << "\033[1;31mAu revoir !\033[0m" << endl;
                 break;
             default:
-                cout << "Choix invalide, veuillez réessayer.";
+                cout << "\033[1;31mChoix invalide, veuillez réessayer.\033[0m" << endl;
                 break;
         }
+    } while (choix != 5);
 }
 
 void presentation::Menu_analyse ( )
 // Algorithme :
 //
 {
+    int choix;
+    do
+    {
+        cout << "\033[1;34m|===================================================|\033[0m\n";
+        cout << "\033[1;34m|               Menu Analyse de Données             |\033[0m\n";
+        cout << "\033[1;34m|===================================================|\033[0m\n";
+        cout << "\033[1;32m|[1] -------- Analyse des données des capteurs------|\033[0m\n";
+        cout << "\033[1;32m|[2] -------- Identifier capteurs non fiables ------|\033[0m\n";
+        cout << "\033[1;32m|[3] -------- Analyser impact des purificateurs-----|\033[0m\n";
+        cout << "\033[1;31m|[4] -------- Retour au menu principal -------------|\033[0m\n";
+        cout << "\033[1;34m|===================================================|\033[0m\n";
+        cout << "Veuillez choisir une valeur entre 1 et 4 : " << endl;
 
+        cin >> choix;
+
+        switch (choix)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                cout << "\033[1;31mRetour au menu principal.\033[0m" << endl;
+                break;
+            default:
+                cout << "\033[1;31mChoix invalide, veuillez réessayer.\033[0m" << endl;
+                break;
+        }
+    } while (choix != 4);
 }
 
 void presentation::Menu_statistiques ( )
 // Algorithme :
 //
 {
+    int choix;
+    do
+    {
+        cout << "\033[1;34m|===================================================|\033[0m\n";
+        cout << "\033[1;34m|               Menu Statistiques                   |\033[0m\n";
+        cout << "\033[1;34m|===================================================|\033[0m\n";
+        cout << "\033[1;32m|[1] ---- Qualité moyenne de l'air par zone --------|\033[0m\n";
+        cout << "\033[1;32m|[2] ---- Qualité moyenne de l'air par point -------|\033[0m\n";
+        cout << "\033[1;31m|[3] ---- Retour au menu principal -----------------|\033[0m\n";
+        cout << "\033[1;34m|===================================================|\033[0m\n";
+        cout << "Veuillez choisir une valeur entre 1 et 3 : " << endl;
 
+        cin >> choix;
+
+        switch (choix)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                cout << "\033[1;31mRetour au menu principal.\033[0m" << endl;
+                break;
+            default:
+                cout << "\033[1;31mChoix invalide, veuillez réessayer.\033[0m"<< endl;
+                break;
+        }
+    } while (choix != 3);
 }
 
 void presentation::Menu_points_utilisateurs ( )
 // Algorithme :
 //
 {
+    int choix;
+    do
+    {
+        cout << "\033[1;34m|===================================================|\033[0m\n";
+        cout << "\033[1;34m|           Menu Points Utilisateurs                |\033[0m\n";
+        cout << "\033[1;34m|===================================================|\033[0m\n";
+        cout << "\033[1;32m|[1] ---- Consulter les points d'un utilisateur ----|\033[0m\n";
+        cout << "\033[1;31m|[2] ---- Retour au menu principal -----------------|\033[0m\n";
+        cout << "\033[1;34m|===================================================|\033[0m\n";
+        cout << "Veuillez choisir une valeur entre 1 et 2 : " << endl;
 
+        cin >> choix;
+
+        switch(choix)
+        {
+            case 1:
+                break;
+            case 2:
+                cout << "\033[1;31mRetour au menu principal.\033[0m" << endl;
+                break;
+            default:
+                cout << "\033[1;31mChoix invalide, veuillez réessayer.\033[0m" << endl;
+                break;
+        }
+    } while (choix != 2);
 }
 
 void presentation::Menu_administration ( )
 // Algorithme :
 //
 {
+    int choix;
+    do
+    {
+        cout << "\033[1;34m|===================================================|\033[0m\n";
+        cout << "\033[1;34m|               Menu Administration                 |\033[0m\n";
+        cout << "\033[1;34m|===================================================|\033[0m\n";
+        cout << "\033[1;32m|[1] ---- Consulter capteurs défaillants -----------|\033[0m\n";
+        cout << "\033[1;32m|[2] ---- Marquer un capteur comme non fiable ------|\033[0m\n";
+        cout << "\033[1;32m|[3] ---- Marquer un utilisateur comme malicieux ---|\033[0m\n";
+        cout << "\033[1;31m|[4] ---- Retour au menu principal -----------------|\033[0m\n";
+        cout << "\033[1;34m|===================================================|\033[0m\n";
+        cout << "Veuillez choisir une valeur entre 1 et 4 : " << endl;
 
+        cin >> choix;
+
+        switch (choix)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                cout << "\033[1;31mRetour au menu principal.\033[0m" << endl;
+                break;
+            default:
+                cout << "\033[1;31mChoix invalide, veuillez réessayer.\033[0m" << endl;
+                break;
+        }
+    } while (choix != 4);
 }
 
 
