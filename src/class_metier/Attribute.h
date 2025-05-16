60d308df328c82f1f344df4f23feded868eaaeb1
danielmassila <daniel.massila-akendengue@insa-lyon.fr>
2025-05-16 16:03:17 +0200

    Mise à jour des classes métiers

--- src/class_metier/Attribute.h
+++ src/class_metier/Attribute.h
@@ -1,24 +1,63 @@
-//
-// Created by aaron on 16/05/2025.
-//
+/*************************************************************************
+                           Attribute  -  description
+                             -------------------
+    début                : 16/05/2025
+    copyright            : (C) 2025 par aaron
+    e-mail               : 
+*************************************************************************/
 
-#ifndef ATTRIBUTE_H
+//---------- Interface de la classe <Attribute> (fichier Attribute.h) ----------------
+#if ! defined ( ATTRIBUTE_H )
 #define ATTRIBUTE_H
 
-class Attribute {
-public:
-    // Constructor
-    Attribute(int id, const string &unit, const string &description)
-        : attributeID(id), unit(unit), description(description) {}
+//--------------------------------------------------- Interfaces utilisées
+#include <string>
+using std::string;
 
-    // Destructor
-    ~Attribute() {}
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <Attribute>
+//
+// Représente un attribut mesuré par un capteur (ex : CO2, température, etc.)
+//------------------------------------------------------------------------
+
+class Attribute
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+
+    // Constructeur
+    Attribute(int id, const string &unit, const string &description);
+    // Mode d'emploi :
+    //  Initialise un attribut avec un identifiant, une unité et une description.
+    //
+    // Contrat :
+    //
+
+    // Destructeur
+    virtual ~Attribute();
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
 protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
     int attributeID;
     string unit;
     string description;
+
 };
 
+//-------------------------------- Autres définitions dépendantes de <Attribute>
 
-
-#endif //ATTRIBUTE_H
+#endif // ATTRIBUTE_H
--- src/class_metier/User.cpp
+++ src/class_metier/User.cpp
@@ -1,21 +1,42 @@
-//
-// Created by aaron on 16/05/2025.
-//
+/*************************************************************************
+                           User  -  description
+                             -------------------
+    début                : 16/05/2025
+    copyright            : (C) 2025 par aaron
+    e-mail               : 
+*************************************************************************/
 
-#include "User.h"
-#include "Sensor.h"
+//---------- Réalisation de la classe <User> (fichier User.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
 #include <iostream>
 #include <string>
 #include <list>
-
 using namespace std;
 
+//------------------------------------------------------ Include personnel
+#include "User.h"
+#include "Sensor.h"
 
-list<Sensor> User::getSensors() {
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+
+list<Sensor> User::getSensors()
+// Algorithme :
+//
+{
     return sensors;
-}
+} //----- Fin de getSensors
 
-list<Sensor> User::compareSensor(Sensor sensor) {
+list<Sensor> User::compareSensor(Sensor sensor)
+// Algorithme :
+//
+{
     list<Sensor> result;
     for (Sensor s : sensors) {
         if (s.getID() == sensor.getID()) {
@@ -23,19 +44,43 @@ list<Sensor> User::compareSensor(Sensor sensor) {
         }
     }
     return result;
-}
+} //----- Fin de compareSensor
 
-double User::getGeoPoint(double latitude, double longitude) {
+double User::getGeoPoint(double latitude, double longitude)
+// Algorithme :
+//
+{
     return 0.0;
-}
+} //----- Fin de getGeoPoint
 
-double User::getGeoZoneMean(double latitude, double longitude, double radius) {
+double User::getGeoZoneMean(double latitude, double longitude, double radius)
+// Algorithme :
+//
+{
     return 0.0;
-}
+} //----- Fin de getGeoZoneMean
 
-// Constructor
+//-------------------------------------------- Constructeur - destructeur
+
 User::User(const string &id, int points)
-    : userID(id), points(points) {}
+// Algorithme :
+//
+    : userID(id), points(points)
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <User>" << endl;
+#endif
+} //----- Fin de User
 
-// Destructor
-User::~User() {}
+User::~User()
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <User>" << endl;
+#endif
+} //----- Fin de ~User
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
