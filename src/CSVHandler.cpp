84c1b85cd9e933acbe8a95638638a586732a3771
r0n <aaron.berton2004@gmail.com>
2025-05-16 17:44:40 +0200

    C'est bon y'a enfin un extract qui marche

--- src/CSVHandler.cpp
+++ src/CSVHandler.cpp
@@ -13,6 +13,8 @@ void CSVHandler::extractSensors() {
     ifstream file(filePath);
     string line;
 
+    unordered_map<unsigned int, Sensor> sensors;
+
     if (file.is_open()) {
         while (getline(file, line)) {
             stringstream ss(line);
@@ -29,7 +31,9 @@ void CSVHandler::extractSensors() {
             double longitude = stod(longitudeStr);
 
             Sensor sensor(id, latitude, longitude, -1);
+            sensors.emplace(id, sensor);
         }
+        this->sensors = sensors;
         file.close();
     } else {
         cout << "Unable to open file" << endl;
